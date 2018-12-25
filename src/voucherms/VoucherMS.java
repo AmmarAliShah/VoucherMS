a384f8a24d5f4ca740ae569fc71e1e708c6e726a
Ammar Shah <syedammarali24@gmail.com>
2018-12-26 00:48:40 +0500

    Basic forms created

--- src/voucherms/VoucherMS.java
+++ src/voucherms/VoucherMS.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package voucherms;
 
 /**
  *
  * @author Ammar Shah
  */
-public class VoucherMS {
+public class VoucherMS{
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Home ob = new Home();
+        ob.setVisible(true);
     }
-    
 }
